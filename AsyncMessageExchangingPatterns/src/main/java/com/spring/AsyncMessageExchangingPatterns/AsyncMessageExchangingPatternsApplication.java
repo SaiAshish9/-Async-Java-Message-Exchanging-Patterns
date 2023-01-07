8608018cf18b482dc2c4deb8573856065ed0f21b
SaiAshish <saiashish3760@gmail.com>
2023-01-07 21:16:48 +0530

    Add files via upload

---
+++ AsyncMessageExchangingPatterns/src/main/java/com/spring/AsyncMessageExchangingPatterns/AsyncMessageExchangingPatternsApplication.java
@@ -0,0 +1,13 @@
+package com.spring.AsyncMessageExchangingPatterns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsyncMessageExchangingPatternsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsyncMessageExchangingPatternsApplication.class, args);
+	}
+
+}
---
+++ AsyncMessageExchangingPatterns/src/test/java/com/spring/AsyncMessageExchangingPatterns/AsyncMessageExchangingPatternsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.AsyncMessageExchangingPatterns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsyncMessageExchangingPatternsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
